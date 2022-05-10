ffeff6496cf28cb33e073dbe7c2ad2a68cc13758
Gustavo de Camargo Campos <contato.wanjalagus@oulook.com.br>
2022-05-10 13:08:57 -0400

    code version v1.03

--- contacts/urls.py
+++ contacts/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('search/', views.search, name='search'),
     path('<int:contact_id>', views.see_contact, name='see_contact'),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,4 +1,6 @@
 from django.core.paginator import Paginator
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 
@@ -24,5 +26,31 @@ def see_contact(request, contact_id):
         raise Http404()
     return render(request, 'contacts/see_contact.html', {
         'contact': contact
+    })
+
+
+def search(request):
+    term = request.GET.get('term')
+
+    if term is None:
+        raise Http404()
+
+
+    fields = Concat('name', Value(' '), 'last_name')
+
+    contacts = Contact.objects.annotate(
+        complete_name = fields
+    ).filter(
+        Q(complete_name__icontains=term) | Q(phone__icontains=term),
+        show = True
+    )
+
+
+    paginator = Paginator(contacts, 5)
+
+    page = request.GET.get('p')
+    contacts = paginator.get_page(page)
+    return render(request, 'contacts/index.html', {
+        'contacts': contacts
     })
 
